b59580d5fec33d6ec21e071c94a6752383cfc4c9
Arth <arth4701@gmail.com>
2023-01-07 23:39:00 +0530

    fixed images path

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><img className="footer-logo" src="src/assets/square-twitter.svg" alt="twitterLogo"/></a>
-            <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><img className="footer-logo" src="src/assets/square-facebook.svg" alt="fbLogo"/></a>
-            <a href="https://www.instagram.com/arth4701/" target="_blank"><img className="footer-logo" src="src/assets/square-instagram.svg" alt="instaLogo"/></a>
-            <a href="https://github.com/ArthSrivastava" target="_blank"><img className="footer-logo" src="src/assets/square-github.svg" alt="githubLogo"/></a>
+            <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><img className="footer-logo" src="/src/assets/square-twitter.svg" alt="twitterLogo"/></a>
+            <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><img className="footer-logo" src="/src/assets/square-facebook.svg" alt="fbLogo"/></a>
+            <a href="https://www.instagram.com/arth4701/" target="_blank"><img className="footer-logo" src="/src/assets/square-instagram.svg" alt="instaLogo"/></a>
+            <a href="https://github.com/ArthSrivastava" target="_blank"><img className="footer-logo" src="/src/assets/square-github.svg" alt="githubLogo"/></a>
         </div>
     )
 }
--- src/info.jsx
+++ src/info.jsx
@@ -3,11 +3,11 @@ import React from "react"
 export default function Info() {
     return (
         <>
-          <img id="dp" src="src/assets/courage.jpg" alt="dp"/> 
+          <img id="dp" src="/src/assets/courage.jpg" alt="dp"/> 
           <h3 id="name">Arth Srivastava</h3>
           <p id="profile">Backend Developer</p>
-          <a href="mailto:arth4701@gmail.com"><button id="emailBtn" className="btn"><img className="btn-logo" src="src/assets/envelope-solid.svg" alt="emailLogo"/> Email</button></a>
-          <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><button id="linkedInBtn" className="btn"><img className="btn-logo" src="src/assets/linkedin.svg" alt="linkedinLogo"/> LinkedIn</button></a>
+          <a href="mailto:arth4701@gmail.com"><button id="emailBtn" className="btn"><img className="btn-logo" src="/src/assets/envelope-solid.svg" alt="emailLogo"/> Email</button></a>
+          <a href="https://www.linkedin.com/in/arth-srivastava/" target="_blank"><button id="linkedInBtn" className="btn"><img className="btn-logo" src="/src/assets/linkedin.svg" alt="linkedinLogo"/> LinkedIn</button></a>
         </>
     )
 }
